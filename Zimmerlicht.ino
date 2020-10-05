1cc156dd805bc8ad197b5a6973addb340d17ff51
Bernhard Stöffler <bernie@bernie-debian>
2020-10-06 01:51:22 +0200

    +fade, +struct s_rgb

--- Zimmerlicht.ino
+++ Zimmerlicht.ino
@@ -63,9 +63,18 @@ char  replyBuffer[UDP_TX_PACKET_MAX_SIZE + 1];       // a string to send back
 //0100 = bu2bl input; 
 //0101 = rainbow;
 
+struct s_rgb {
+  int r;
+  int g;
+  int b;
+  int brt;
+};
+
 char mode = 0;
-int r, g, b;
-int brt = 100;
+struct s_rgb rgbC; //current
+struct s_rgb rgbN; //new
+
+int fade = 1000; //fade duration in milliseconds
 
 int packetCount = 0;
 unsigned long previousTime = 0;
@@ -74,7 +83,7 @@ WiFiUDP Udp;
 
 void(* resetFunc) (void) = 0;
 
-int getPacket()
+int getPacket(struct s_rgb *rgbN, struct s_rgb rgbC)
 {  
   // if there's data available, read a packet
   int packetSize = Udp.parsePacket();
@@ -99,28 +108,42 @@ int getPacket()
     //parse Packet
     switch(packetBuffer[0]){
       case 0: //reboot
-      resetFunc();
+        resetFunc();
       break;
+      
       case 1: //modify color
-      mode = 1;
-      r = packetBuffer[1]; g = packetBuffer[2]; b = packetBuffer[3];
-      if(brt==0)
-        brt = 100;      
+        mode = 1;
+        rgbN->r = packetBuffer[1];
+        rgbN->g = packetBuffer[2];
+        rgbN->b = packetBuffer[3];
+        if(rgbC.brt==0){ //if the light is off, a new color should turn it on
+          rgbN->brt = 100;      
+        }else{
+          rgbN->brt = rgbC.brt;
+        }
+          
       break;
+      
       case 2: //absolute brightness
-      mode = 1;
-      brt = packetBuffer[1];
+        mode = 1;
+        *rgbN = rgbC;
+        rgbN->brt = packetBuffer[1];
       break;
+      
       case 3: //relative brightness
-      mode = 1;
-      (!packetBuffer[2]) ? brt+=packetBuffer[1] : brt-=packetBuffer[1];
-      if(brt<0)   brt = 0;
-      if(brt>100) brt = 100;
+        mode = 1;
+        *rgbN = rgbC;
+        (!packetBuffer[2]) ? rgbN->brt+=packetBuffer[1] : rgbN->brt-=packetBuffer[1]; //add or subtract
+        if(rgbN->brt<0)   rgbN->brt = 0;   //clip
+        if(rgbN->brt>100) rgbN->brt = 100;
       break;
+      
       case 4: //bu2bl2
-      mode = 4;
-      brt = 100;
-      r = packetBuffer[1]; g = packetBuffer[2]; b = packetBuffer[3];
+        mode = 4;
+        rgbN->brt = 100;
+        rgbN->r = packetBuffer[1];
+        rgbN->g = packetBuffer[2];
+        rgbN->b = packetBuffer[3];
       break;
     }
     
@@ -129,31 +152,56 @@ int getPacket()
   }
   return packetSize;
 }
-void updateColors()
+void updateColors(struct s_rgb rgbN, struct s_rgb rgbC, int fade)
 {
+  //outdated! too lazy to fix since udpreplies dont work anyways
   #ifdef UDPREPLIES
     char string[64];
     snprintf(string, 64, "c: %u, %u, %u, %u\n", (r*brt)/255, (g*brt)/255, (b*brt)/255, (w*brt)/255);
     sendPacket(string);
   #endif
 
-  //TODO: edge cases when rgb = 0 maybe (or is that for the parser?)
 
-  #ifdef L_ZIMMER
-    if(r==g&&g==b){
-      analogWrite(LEDW, (r*brt)/100);
-      digitalWrite(LEDR, LOW); digitalWrite(LEDG, LOW); digitalWrite(LEDB, LOW);
-    }else{
-      analogWrite(LEDR, (r*brt)/100);
-      analogWrite(LEDG, (g*brt)/100);
-      analogWrite(LEDB, (b*brt)/100);
-      digitalWrite(LEDW, LOW);
+  if(fade){ //todo: actual millis() calculation (if not too expensive)
+    for(int i=0; i<fade; i++){
+      #ifdef L_ZIMMER
+        if(rgbN.r==rgbN.g&&rgbN.g==rgbN.b){ //white LED needs extra stuff
+          analogWrite(LEDW, (((rgbC.r*rgbC.brt)/100) * (fade-i)  +  ((rgbN.r*rgbN.brt)/100) * i)/fade);
+          digitalWrite(LEDR, LOW); 
+          digitalWrite(LEDG, LOW); 
+          digitalWrite(LEDB, LOW);
+        }else{ //update colors regularly
+          analogWrite(LEDR, (((rgbC.r*rgbC.brt)/100) * (fade-i)  +  ((rgbN.r*rgbN.brt)/100) * i)/fade);
+          analogWrite(LEDG, (((rgbC.g*rgbC.brt)/100) * (fade-i)  +  ((rgbN.g*rgbN.brt)/100) * i)/fade);
+          analogWrite(LEDB, (((rgbC.b*rgbC.brt)/100) * (fade-i)  +  ((rgbN.b*rgbN.brt)/100) * i)/fade);
+          digitalWrite(LEDW, LOW);
+        }
+      #else //update colors regularly
+        analogWrite(LEDR, (((rgbC.r*rgbC.brt)/100) * (fade-i)  +  ((rgbN.r*rgbN.brt)/100) * i)/fade);
+        analogWrite(LEDG, (((rgbC.g*rgbC.brt)/100) * (fade-i)  +  ((rgbN.g*rgbN.brt)/100) * i)/fade);
+        analogWrite(LEDB, (((rgbC.b*rgbC.brt)/100) * (fade-i)  +  ((rgbN.b*rgbN.brt)/100) * i)/fade);
+      #endif
+      delay(1); //1 millisecond delay, total delay = fade;
     }
-  #else
-    analogWrite(LEDR, (r*brt)/100);
-    analogWrite(LEDG, (g*brt)/100);
-    analogWrite(LEDB, (b*brt)/100);
-  #endif
+  }else{ //switch color immediately
+    #ifdef L_ZIMMER // white LED needs extra stuff
+      if(rgbN.r==rgbN.g&&rgbN.g==rgbN.b){
+        analogWrite(LEDW, (rgbN.r*rgbN.brt)/100);
+        digitalWrite(LEDR, LOW); 
+        digitalWrite(LEDG, LOW); 
+        digitalWrite(LEDB, LOW);
+      }else{ //update colors regularly
+        analogWrite(LEDR, (rgbN.r*rgbN.brt)/100);
+        analogWrite(LEDG, (rgbN.g*rgbN.brt)/100);
+        analogWrite(LEDB, (rgbN.b*rgbN.brt)/100);
+        digitalWrite(LEDW, LOW);
+      }
+    #else //update colors regularly
+      analogWrite(LEDR, (rgbN.r*rgbN.brt)/100);
+      analogWrite(LEDG, (rgbN.g*rgbN.brt)/100);
+      analogWrite(LEDB, (rgbN.b*rgbN.brt)/100);
+    #endif
+  }
 }
 
 void sendPacket(char*string)
@@ -236,19 +284,23 @@ void loop()
   switch(mode)
   { 
     case 0: // Boot/Debug
-      brt = 255;
-      r = 255; g = 255; b = 255;
-      updateColors();
+      rgbC = { 255, 255, 255, 100 };
+      rgbN = rgbC;
+      updateColors(rgbN, rgbC, 0);
       mode = 1;
     break;
     case 1: // Room Light
-      if(getPacket())
-        updateColors();
+      if(getPacket(&rgbN, rgbC)){
+        updateColors(rgbN, rgbC, fade);
+        rgbC = rgbN;
+      }
     break;
     case 4: // Bu2Bl2
-      if(getPacket())
-        updateColors();
-
+      if(getPacket(&rgbN, rgbC)){
+        updateColors(rgbN, rgbC, 0);
+        rgbC = rgbN;
+      }
+      
       if(millis()-previousTime > 5000 && packetCount){
         Serial.printf("Haven't received Packet for five seconds, rebooting into light mode\n");
         delay(10);
