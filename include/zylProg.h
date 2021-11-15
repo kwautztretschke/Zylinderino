721f029c0af861fcea5891d522fc5afba571255b
Norbert <norbert@nosy>
2021-11-15 11:09:45 +0100

    zylProg: made manager static
    ... so it works without the need of creating an instance first.
    
    P.S. I only tested if it compiles, not if it actually works like this :-)

--- include/zylProg.h
+++ include/zylProg.h
@@ -12,18 +12,24 @@
 #ifndef __ZYLPROG_H_
 #define __ZYLPROG_H_
 
+#ifdef ARDUINO
 #include <FastLED.h>
+#else
+typedef char CRGB;
+#include	<stdlib.h>
+#include	<stdint.h>
+#endif
 #include "config.h"
 
-class zylProgManager; // forward declaration, needed for zylProg(zpm)
+//class zylProgManager; // forward declaration, needed for zylProg(zpm)
 
 class zylProg{
 protected:
 	CRGB					m_FB[X_RES][Y_RES];
 public:
-	zylProg*				m_pNext = NULL;
+	zylProg					*m_pNext = NULL;
 
-	;						zylProg(zylProgManager zpm);
+	;						zylProg();
 	virtual int 			init(){}
 	virtual void          	activate(){};
 	virtual void          	render() = 0;    // * Virtual function, so must be redefined
@@ -34,45 +40,60 @@ public:
 
 class zylProgManager{
 private:
-	int						m_Count = 0;
-	zylProg*				m_pHead=NULL;
-	zylProg*				m_pActive=NULL;
+	static int					g_Count;
+	static zylProg*				g_pHead;
+	static zylProg*				g_pActive;
 public:
-	void					add(zylProg* ptr);
-	void					focus(int index);
-	void					renderPrograms();
-	void					composite(CRGB** fb);
-	void					input(uint8_t x, uint8_t y, uint8_t z);
+	static void					add(zylProg *ptr);
+	static void					focus(int index);
+	static void					renderPrograms();
+	static void					composite(CRGB *fb);
+	static void					input(uint8_t x, uint8_t y, uint8_t z);
 };
 
-void zylProgManager::add(zylProg* ptr){
-	if (m_pHead == NULL){ //first program
-		m_pHead = ptr;
-		m_pActive = ptr;
+
+/* move to cpp *****************/
+
+//init "static members" (i.e. global class variables)
+
+int zylProgManager::g_Count=0;
+zylProg *zylProgManager::g_pHead=NULL;
+zylProg *zylProgManager::g_pActive=NULL;
+
+// implement "static methods" (i.e. global functions)
+
+void zylProgManager::add(zylProg* ptr)
+{
+	if (g_pHead == NULL){ //first program
+		g_pHead = ptr;
+		g_pActive = ptr;
 	}else{
-		zylProg* cur = m_pHead;
+		zylProg* cur = g_pHead;
 		while(cur->m_pNext != NULL)
 			cur = cur->m_pNext;
 		cur->m_pNext = ptr;
 	}
-	m_Count++;
+	g_Count++;
 }
 
-void zylProgManager::focus(int index){
-	zylProg* ptr = m_pHead;
+void zylProgManager::focus(int index)
+{
+	zylProg* ptr = g_pHead;
 	for(int i=0; i<index; i++)
 		ptr = ptr->m_pNext;
-	m_pActive = ptr;
-	m_pActive->activate();
+	g_pActive = ptr;
+	g_pActive->activate();
 }
 
-void zylProgManager::input(uint8_t x, uint8_t y, uint8_t z){
-	m_pActive->input(x, y, z);
+void zylProgManager::input(uint8_t x, uint8_t y, uint8_t z) // I would strongly recommend int x,
+															// but there are bazillions of flys ...
+{
+	g_pActive->input(x, y, z);
 }
-//static zylProgManager zpm;	//? I hope this works? should be only one zpm overall, the same for each program
 
-zylProg::zylProg(zylProgManager zpm) { // automatically add new zylProg to linked list
-	zpm.add(this);
+zylProg::zylProg()
+{ // automatically add new zylProg to linked list
+	zylProgManager::add(this);
 };
 
 
