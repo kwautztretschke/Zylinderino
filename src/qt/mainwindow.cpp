6c9a38201e4689ec4e0a67c1f5f13fe26b7b3f8d
Bernhard Stöffler <bernhard.stoeffler@tum.de>
2022-06-28 01:55:00 +0200

    some comments

--- src/qt/mainwindow.cpp
+++ src/qt/mainwindow.cpp
@@ -9,27 +9,34 @@ MainWindow::~MainWindow(){
 }
 
 int MainWindow::setupUi(){
+	// main window properties
 	this->setWindowTitle("Zylator - A ZylOS Emulator");
 
+	// central widget (fills entire window)
 	centralWidget = new QWidget(this);
 	this->setCentralWidget(centralWidget);
 
+	// vertical layout to arrange items in full rows
 	verticalLayout = new QVBoxLayout(centralWidget);
 	verticalLayout->setSpacing(6);
 	verticalLayout->setContentsMargins(11, 11, 11, 11);
 
+	// the graphics box where the framebuffer is displayed
 	graphicsView = new QGraphicsView(centralWidget);
 	verticalLayout->addWidget(graphicsView);
 
+	// a dropdown box to select programs
 	programSelector = new QComboBox(centralWidget);
 	programSelector->addItem(QString("dummy"));
 	verticalLayout->addWidget(programSelector);
 
+	// a stacked widget, where new zylProg input widgets are added
 	inputStack = new QStackedWidget(centralWidget);
 	verticalLayout->addWidget(inputStack);
 	dummyInputPage = new QWidget(); //* das wird hier als dummy hinzugeböllert
 	inputStack->addWidget(dummyInputPage);
 
+	// a menu bar. why not.
 	menuBar = new QMenuBar(this);
 	menuBar->setGeometry(QRect(0, 0, 348, 19));
 	this->setMenuBar(menuBar);
