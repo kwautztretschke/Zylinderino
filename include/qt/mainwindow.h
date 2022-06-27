d8808a5d5eae7b0488f07ff818f2921c8f3e6ab8
Bernhard Stöffler <bernie@bernie-tablet>
2022-06-27 23:43:00 +0200

    +UI skeleton

--- include/qt/mainwindow.h
+++ include/qt/mainwindow.h
@@ -1,13 +1,31 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <QtCore/QVariant> // braucht man das?
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QApplication> // und das?
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QGraphicsView>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QStackedWidget>
+#include <QtWidgets/QMenuBar>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+private:
+	int				setupUi();
 
 public:
+    QWidget 		*centralWidget;
+    QVBoxLayout 	*verticalLayout;
+    QGraphicsView 	*graphicsView;
+    QComboBox 		*programSelector;
+    QStackedWidget 	*inputStack;
+    QWidget 		*dummyInputPage;
+    QMenuBar 		*menuBar; // naja brauchmer das
+
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 };
--- src/qt/mainwindow.cpp
+++ src/qt/mainwindow.cpp
@@ -1,11 +1,38 @@
 #include "qt/mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{
+    : QMainWindow(parent){
+	setupUi();
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow(){
 }
 
+int MainWindow::setupUi(){
+	this->setWindowTitle("Zylator - A ZylOS Emulator");
+
+	centralWidget = new QWidget(this);
+	this->setCentralWidget(centralWidget);
+
+	verticalLayout = new QVBoxLayout(centralWidget);
+	verticalLayout->setSpacing(6);
+	verticalLayout->setContentsMargins(11, 11, 11, 11);
+
+	graphicsView = new QGraphicsView(centralWidget);
+	verticalLayout->addWidget(graphicsView);
+
+	programSelector = new QComboBox(centralWidget);
+	programSelector->addItem(QString("dummy"));
+	verticalLayout->addWidget(programSelector);
+
+	inputStack = new QStackedWidget(centralWidget);
+	verticalLayout->addWidget(inputStack);
+	dummyInputPage = new QWidget(); //* das wird hier als dummy hinzugeböllert
+	inputStack->addWidget(dummyInputPage);
+
+	menuBar = new QMenuBar(this);
+	menuBar->setGeometry(QRect(0, 0, 348, 19));
+	this->setMenuBar(menuBar);
+
+	return 0; // TODO sensible errorhandling
+}
