5e44851632b2ba6e9f7e89d888662b9e76ec709c
Victoria Stöffler <victoria.stoeffler@tum.de>
2023-09-17 10:12:16 +0200

    Added library config files

---
+++ config/ZylOsConfig.h
@@ -0,0 +1,6 @@
+#ifndef __ZYLOSCONFIG_H__ 
+#define __ZYLOSCONFIG_H__
+
+#define Z_MONOCHROME
+
+#endif // __ZYLOSCONFIG_H__
---
+++ config/wificredentials.h
@@ -0,0 +1,14 @@
+#ifndef __WIFICREDENTIALS_H__
+#define __WIFICREDENTIALS_H__
+
+#include <ESP8266WiFi.h>
+
+namespace wificredentials{
+	const String 	ssid = 			"dummerhotspot";
+	const String	password = 		"Amogus187";
+	const IPAddress mqttServerIp = 	IPAddress(10,69,0,69);
+	const String	deviceName = 	"tollerESP";
+}
+
+
+#endif // __WIFICREDENTIALS_H__
