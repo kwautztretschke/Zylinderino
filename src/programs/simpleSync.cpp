72dab3d3771575c6f10717475a20824dc9324395
Victoria Stöffler <victoria.stoeffler@tum.de>
2023-09-26 15:03:09 +0200

    made simpleSync a ArtnetProgram

--- src/programs/simpleSync.cpp
+++ src/programs/simpleSync.cpp
@@ -2,35 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 
-static class : public Program{
+static class : public ArtnetProgram{
+private:
+	char m_ArtNetHistory[32] = {0};
 public:
-	int m_Channel = 0;
-	int m_ColorIndex = 0;
-	int m_Brightness = 0;
-	using Program::Program;
+	using ArtnetProgram::ArtnetProgram;
 	int init(){
 		m_Name = "simpleSync";
+		m_pArtNetHistory = m_ArtNetHistory;
+		m_ArtNetHistorySize = 32;
 		return 0;
-	}
-	int input(char* key, char* value){
-		if(!strcmp(key, "channel"))
-			m_Channel = strtol(value, NULL, 10);
-		else if(!strcmp(key, "colorindex"))	
-			m_ColorIndex = strtol(value, NULL, 10);
-		else{
-			// wrong input
-			return 1;
-		}
-		return 0;
-	}
-	void artnet(const uint8_t* data, const uint16_t size){
-		if(m_Channel >= size)
-			return;
-		m_Brightness = data[m_Channel];
 	}
 	void render(long ms){
-		CRGB c = ProgramManager::getColor(m_ColorIndex);
-		c.nscale8(m_Brightness);
+		CRGB c = getColor();
+		c.nscale8(getModulator());
 		for (int i=0;i<FB_SIZE;i++){
 			m_FB[i] = c;
 		}
