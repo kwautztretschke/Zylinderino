c661d92d2a8032ff7c0dcea2a1e388c45626dd8a
Victoria Stöffler <victoria.stoeffler@tum.de>
2023-10-08 02:23:07 +0200

    added zylOs changes for 2D framebuffer, merged configs, removed zylProg.cpp

--- config/ZylOsConfig.h
+++ config/ZylOsConfig.h
@@ -1,9 +1,11 @@
 #ifndef __ZYLOSCONFIG_H__ 
 #define __ZYLOSCONFIG_H__
 
-#define Z_DEVICE_NAME "tollerESP"
-#define Z_WS2812B
-#define FB_SIZE 30
+#define Z_DEVICE_NAME "zylinder"
+#define Z_ZYLINDER
+// #define FB_SIZE 280
+#define X_RES 35
+#define Y_RES 8
 #define Z_DEFAULT_PROGRAM "solidColor"
 
 #endif // __ZYLOSCONFIG_H__
